afe0f414dec3b30cce35c2b0b834f8caa52bc2c3
JayeshGurav <jayeshgurav1419@gmail.com>
2024-10-29 10:01:06 +0530

    Add files via upload

---
+++ EC21/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+#Configure the AWS provider
+provider "aws" {
+region = "ap-south-1"  
+}
+
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number   
+    default = 8080  
+}
+
+resource "aws_security_group" "instance_sg" {
+    name = "terraform-example-instance-sg"  
+    
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    }
+
+
+
+resource "aws_launch_configuration" "ec21" {
+    image_id = "ami-04a37924ffe27da53"
+    instance_type = "t2.micro"
+    #security_groups = [aws_security_group.instance_sg]
+
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello World" > index.html
+        nohup busybox httpd -f -p ${var.server_port} &
+        EOF
+  
+}
